3283d20260f7c54a3477a9d14ee18dc225da8c82
dstan82 <53448896+dstan82@users.noreply.github.com>
2020-05-28 21:36:27 +0300

    Update Collatz_Conjecture.py

--- Collatz Conjecture/Collatz_Conjecture.py
+++ Collatz Conjecture/Collatz_Conjecture.py
@@ -1,9 +1,3 @@
-'''
-Collatz Conjecture - Start with a number n > 1.
-Find the number of steps it takes to reach one using the following process:
-If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1.
-'''
-
 num = int(input('Enter a number:'))
 
 
@@ -20,6 +14,7 @@ def collatzRecur(num):
 
 print(collatzRecur(num))
 
+# Alternative method
 ''''
 def collatzRecur(curNum, count=0):
     $This recursively solves the Collatz Conjecture
